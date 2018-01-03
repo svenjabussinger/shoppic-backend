784757491c9729d7cfc5d2a779c39dbd86b4111d
Svenja Bussinger <svenja.bussinger@gmail.com>
2018-01-03 11:18:28 +0100

    user routes connecting to mongodb

--- shoppic-backend/routes/users.js
+++ shoppic-backend/routes/users.js
@@ -1,17 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var ObjectID = require('mongodb').ObjectID;
 
 // Route for all users
 router.route('/')
     .get(function (req, res) {
+        //res.render('users', {user: 'all shoppic user'})
+
         var collection = req.db.collection('users');
         collection.find({}).toArray(function(err, docs){
-            console.log(docs);
+           res.json(docs);
         });
-
-        res.render('users', {user: 'all shoppic user'})
+;
     })
     .post(function (req, res) {
+        var values = req.body;
+
+        var collection = req.db.collection('users');
+        collection.insertOne(values)
+            .then(function(result){
+               console.log(result);
+            });
+
        res.render('users', {user: 'new shoppic user'})
     });
 
@@ -19,14 +29,40 @@ router.route('/')
 router.route('/:user_ID')
     .get(function (req, res) {
         var params = req.params;
-        console.log(params.user_ID);
-        res.render('users', {user: 'get one shoppic user'})
+        var uid = params.user_ID;
+        var objectID = new ObjectID(uid);
+
+
+        var collection = req.db.collection('users');
+        collection.find({_id: objectID}).toArray(function(err, docs){
+            res.json(docs);
+        });
+        // res.render('users', {user: 'get one shoppic user'})
     })
     .put(function (req, res) {
-        res.render('users', {user: 'edited one shoppic user'})
+        var params = req.params;
+        var uid = params.user_ID;
+        var objectID = new ObjectID(uid);
+        var values = req.body;
+
+        var collection = req.db.collection('users');
+        collection.updateOne({_id: objectID}, {$set: values})
+            .then(function(result){
+                console.log(result);
+            });
+        //res.render('users', {user: 'edited one shoppic user'})
     })
     .delete(function (req, res) {
-        res.render('users', {user: 'deleted one shoppic user'})
+        var params = req.params;
+        var uid = params.user_ID;
+        var objectID = new ObjectID(uid);
+
+        var collection = req.db.collection('users');
+        collection.deleteOne({_id: objectID})
+            .then(function (result) {
+                console.log(result);
+            });
+        //res.render('users', {user: 'deleted one shoppic user'})
     });
 
 /* GET users listing.
